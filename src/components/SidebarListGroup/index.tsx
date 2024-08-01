4fb07b9c0c51c6feac6d429a66e436cb9cfe4d01
William Haw <me@williamhaw.com>
2024-08-01 18:25:28 +0800

    Add shop and shield icons to SidebarListGroup

--- src/components/SidebarListGroup/index.tsx
+++ src/components/SidebarListGroup/index.tsx
@@ -1,6 +1,8 @@
 import { MarketplaceDetail } from "@/lib/client/models/models";
 import Image from "next/image";
 import React, { Dispatch, SetStateAction } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShield, faShop } from "@fortawesome/free-solid-svg-icons";
 
 export interface SidebarListGroupProps {
   selectedCompanyId: string;
@@ -22,14 +24,18 @@ export const SidebarListGroup: React.FC<SidebarListGroupProps> = (
         <div className="text-sm">The Hot100</div>
         <div className="text-xs">{props.companies.length} results</div>
       </div>
-      <div className="p-3">
-        <div>{/* image  */}</div>
-        <div className="text-purple-600 text-base font-semibold">
-          Show the marketplace where you&apos;d be a top seller
+      <div className="flex p-3 gap-3 items-center">
+        <span className="w-16 text-gray-400 text-center">
+          <FontAwesomeIcon icon={faShop} size="2x" />
+        </span>
+        <div>
+          <div className="text-purple-600 text-base font-semibold">
+            Show the marketplace where you&apos;d be a top seller
+          </div>
+          <button className="rounded-2xl border border-purple-600 px-4 py-1 text-sm text-purple-600">
+            Sign in to show top picks for you
+          </button>
         </div>
-        <button className="rounded-2xl border border-purple-600 px-4 py-1 text-sm text-purple-600">
-          Sign in to show top picks for you
-        </button>
       </div>
       <div className="flex flex-col">
         {props.companies.map((company) => (
@@ -46,9 +52,11 @@ export const SidebarListGroup: React.FC<SidebarListGroupProps> = (
             <Image src={company.logoLink} alt={""} width={70} height={70} />
             <div className="flex flex-col">
               <div className="text-purple-600 text-base font-semibold">
-                {company.name}
+                {company.name} <span className="text-gray-600 text-xs"><FontAwesomeIcon icon={faShield} /></span>
               </div>
-              <div className="text-sm">{company.type}</div>
+              <div className="text-sm">
+                <FontAwesomeIcon icon={faShop} /> {company.type}
+              </div>
               <div className="text-xs">
                 {company.activeBuyersCount.toLocaleString()} active buyers
               </div>
