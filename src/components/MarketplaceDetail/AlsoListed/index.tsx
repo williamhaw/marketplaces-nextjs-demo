89fe8382ba98c3cf43765741c24c473806170617
William Haw <me@williamhaw.com>
2024-08-01 17:41:58 +0800

    Fix text and spacing for AlsoListedComponent

--- src/components/MarketplaceDetail/AlsoListed/index.tsx
+++ src/components/MarketplaceDetail/AlsoListed/index.tsx
@@ -22,39 +22,63 @@ export const AlsoListedComponent: React.FC<AlsoListedProps> = (
   };
 
   return (
-    <div className="p-4 border rounded-2xl">
+    <div className="p-8 border rounded-2xl">
       <div>
         <div>
           <div>
-            <div>Where companies on this marketplace also list</div>
-            <div>Based on the most popular companies</div>
+            <div className="font-semibold">
+              Where companies on this marketplace also list
+            </div>
+            <div className="text-sm font-light">Based on the most popular companies</div>
           </div>
-
+          <div className="p-3"></div>
           <div className="">
-            <div></div>
             <div className="flex gap-2">
               <div className="grow"></div>
-              <button onClick={showPrevious}>&lt; Previous</button>
-              <button onClick={showNext}>Next &gt;</button>
+              <button
+                onClick={showPrevious}
+                className={
+                  "text-sm " + (windowIndex === 0 ? "text-gray-400" : "")
+                }
+              >
+                &lt; Previous
+              </button>
+              <button
+                onClick={showNext}
+                className={
+                  "text-sm " +
+                  (windowIndex === props.alsoListed.length - 1
+                    ? "text-gray-400"
+                    : "")
+                }
+              >
+                Next &gt;
+              </button>
             </div>
           </div>
         </div>
+        <div className="py-5"></div>
         <hr className="solid" />
+        <div className="py-5"></div>
         <div className="flex py-3">
           {props.alsoListed
             .filter((v, i) => i >= windowIndex && i < windowIndex + windowSize)
             .map((marketplace) => (
-              <div key={marketplace.name}>
+              <div key={marketplace.name} className="w-1/2">
                 <Image
                   src={marketplace.logoLink}
                   alt={marketplace.name + " logo"}
                   width={40}
                   height={40}
                 />
-                <div>{marketplace.name}</div>
-                <div>{marketplace.type}</div>
+                <div className="font-semibold">{marketplace.name}</div>
+                <div className="text-xs">{marketplace.type}</div>
+                <div className="py-5"></div>
                 <hr className="solid w-1/2" />
-                <div>Show companies like you who list here</div>
+                <div className="py-5"></div>
+                <div className="text-xs text-gray-400">
+                  Show companies like you who list here
+                </div>
               </div>
             ))}
         </div>
