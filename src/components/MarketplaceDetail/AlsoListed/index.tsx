1516418c07790c6b6adcd54360ad0f0da1e8c39b
William Haw <me@williamhaw.com>
2024-08-01 16:59:27 +0800

    Implement carousel in AlsoListedComponent

--- src/components/MarketplaceDetail/AlsoListed/index.tsx
+++ src/components/MarketplaceDetail/AlsoListed/index.tsx
@@ -1,5 +1,5 @@
 import { AlsoListed } from "@/lib/client/models/models";
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 
 export interface AlsoListedProps {
@@ -9,6 +9,18 @@ export interface AlsoListedProps {
 export const AlsoListedComponent: React.FC<AlsoListedProps> = (
   props: AlsoListedProps
 ) => {
+  const windowSize = 2;
+  const [windowIndex, setWindowIndex] = useState(0);
+
+  const showNext = () => {
+    if (windowIndex < props.alsoListed.length - 1)
+      setWindowIndex(windowIndex + 1);
+  };
+
+  const showPrevious = () => {
+    if (windowIndex > 0) setWindowIndex(windowIndex - 1);
+  };
+
   return (
     <div className="p-4 border rounded-2xl">
       <div>
@@ -22,27 +34,29 @@ export const AlsoListedComponent: React.FC<AlsoListedProps> = (
             <div></div>
             <div className="flex gap-2">
               <div className="grow"></div>
-              <button>&lt; Previous</button>
-              <button>Next &gt;</button>
+              <button onClick={showPrevious}>&lt; Previous</button>
+              <button onClick={showNext}>Next &gt;</button>
             </div>
           </div>
         </div>
         <hr className="solid" />
         <div className="flex py-3">
-          {props.alsoListed.map((marketplace) => (
-            <div key={marketplace.name}>
-              <Image
-                src={marketplace.logoLink}
-                alt={marketplace.name + " logo"}
-                width={40}
-                height={40}
-              />
-              <div>{marketplace.name}</div>
-              <div>{marketplace.type}</div>
-              <hr className="solid w-1/2" />
-              <div>Show companies like you who list here</div>
-            </div>
-          ))}
+          {props.alsoListed
+            .filter((v, i) => i >= windowIndex && i < windowIndex + windowSize)
+            .map((marketplace) => (
+              <div key={marketplace.name}>
+                <Image
+                  src={marketplace.logoLink}
+                  alt={marketplace.name + " logo"}
+                  width={40}
+                  height={40}
+                />
+                <div>{marketplace.name}</div>
+                <div>{marketplace.type}</div>
+                <hr className="solid w-1/2" />
+                <div>Show companies like you who list here</div>
+              </div>
+            ))}
         </div>
       </div>
     </div>
