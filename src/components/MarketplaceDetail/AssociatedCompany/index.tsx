79ff77d34cc986f19751d19b4cd48e0a957f7968
William Haw <me@williamhaw.com>
2024-08-01 17:56:02 +0800

    Fix text in AssociatedCompanyComponent

--- src/components/MarketplaceDetail/AssociatedCompany/index.tsx
+++ src/components/MarketplaceDetail/AssociatedCompany/index.tsx
@@ -13,46 +13,54 @@ export const AssociatedCompanyComponent: React.FC<AssociatedCompanyProps> = (
   return (
     <>
       {props.company && (
-        <div className="p-4 border rounded-2xl">
-          <h3>Associated Company</h3>
-          <div className="flex items-center gap-x-3">
-            <Image
-              src={props.company.logoLink}
-              alt={""}
-              width={40}
-              height={40}
-            />
-            <div className="flex flex-col">
-              <span>{props.company.name}</span>
-              <span>
-                {props.company.distributionCount} marketplaces{" "}
-                <abbr title="Number of marketplaces where this company is listed.">
-                  ⓘ
-                </abbr>
-              </span>
+        <div className="p-8 border rounded-2xl">
+          <div className="flex flex-col gap-6">
+            <div className="text-lg font-semibold">Associated Company</div>
+            <div className="flex items-center gap-x-3">
+              <Image
+                src={props.company.logoLink}
+                alt={""}
+                width={40}
+                height={40}
+              />
+              <div className="flex flex-col">
+                <span className="font-semibold">{props.company.name}</span>
+                <span className="text-sm">
+                  {props.company.distributionCount} marketplaces{" "}
+                  <abbr title="Number of marketplaces where this company is listed.">
+                    ⓘ
+                  </abbr>
+                </span>
+              </div>
             </div>
+            <div>
+              <div className="font-semibold text-sm">Website</div>
+              <Link href={props.company.website}>
+                <span className="text-xs">{props.company.website}</span>
+              </Link>
+            </div>
+            <div>
+              <div className="font-semibold text-sm">Industry</div>
+              <div className="text-xs">{props.company.industry}</div>
+            </div>
+            <div>
+              <div className="font-semibold text-sm">Company Size</div>
+              <div className="text-xs">
+                {companySizeToText(props.company.companySize)}
+              </div>
+            </div>
+            <div>
+              <div className="font-semibold text-sm">About</div>
+              <div className="text-xs">{props.company.about}</div>
+            </div>
+            <hr className="w-full" />
           </div>
           <div>
-            <div>Website</div>
-            <Link href={props.company.website}>{props.company.website}</Link>
-          </div>
-          <div>
-            <div>Industry</div>
-            <div>{props.company.industry}</div>
-          </div>
-          <div>
-            <div>Company Size</div>
-            <div>{companySizeToText(props.company.companySize)}</div>
-          </div>
-          <div>
-            <div>About</div>
-            <div>{props.company.about}</div>
-          </div>
-          <hr className="w-full" />
-          <div className="flex pt-3">
-            <div className="grow"></div>
-            <button className="text-purple-700">Show all details</button>
-            <div className="grow"></div>
+            <div className="flex pt-3">
+              <div className="grow"></div>
+              <button className="text-purple-700">Show all details</button>
+              <div className="grow"></div>
+            </div>
           </div>
         </div>
       )}
