229d9a9711437aa25605890ad5b42c47a30a5273
William Haw <me@williamhaw.com>
2024-08-01 16:52:09 +0800

    Fix also-listed API route

--- src/app/api/bff/also-listed/route.ts
+++ src/app/api/bff/also-listed/route.ts
@@ -2,7 +2,11 @@ import createClient from "@/lib/client/api/mock.client";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
+  const id = request.nextUrl.searchParams.get("id");
   const client = await createClient();
-  const body = await client.getAlsoListed();
+
+  if (!id) return NextResponse.json({});
+
+  const body = await client.getAlsoListed(id);
   return NextResponse.json(body);
 }
